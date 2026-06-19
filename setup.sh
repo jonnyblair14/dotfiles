123ae611049293227c9c12d3c843301b0ba9504c
jonnyblair14 <jonny@alpha9o1.com>
2026-06-19 16:47:42 +0100

    fixed function call case

--- setup.sh
+++ setup.sh
@@ -105,7 +105,7 @@ if [[ ${nvimFlag} = 'true' ]]; then
 	pipx install neovim-remote
 
 	# tree-sitter install
-	Ensure-rustup
+	Ensure-Rustup
 	curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
 	rustup update
